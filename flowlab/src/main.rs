a618261b3f43a86704de13c2572b6b12ded542be
Samuel Huff Dieterich <samuel.dieterich@ufrgs.br>
2023-10-13 15:44:41 -0300

    ✅ Add tests the proper way

--- flowlab/src/main.rs
+++ flowlab/src/main.rs
@@ -1,14 +1,2 @@
-use flowlab_lib::device::{Device, Protocols, TCP};
-use flowlab_lib::instruction::base::Instruction;
-use flowlab_lib::parser::parse;
-use std::path::Path;
-
 #[tokio::main]
-async fn main() {
-    // let device_path = Path::new("../config/devices/devices.yaml");
-    // let devices: Vec<Device<Protocols>> = parse(device_path).await.unwrap();
-    // println!("{:#?}", devices);
-    let instruction_path = Path::new("../config/instructions/612.yaml");
-    let instructions: Vec<Instruction> = parse(instruction_path).await.unwrap();
-    println!("{:#?}", instructions);
-}
+async fn main() {}
