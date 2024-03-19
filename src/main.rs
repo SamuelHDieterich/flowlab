658267b2a5e6bd77c69a303068980f8c87ab5cf0
Samuel Huff Dieterich <samuel.dieterich@ufrgs.br>
2024-03-18 23:44:43 -0300

    🚧 Start CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,35 @@
-use flowlab::{device, instruction, parser};
+// use flowlab::{device, instruction, parser};
+// extern crate clap;
+use clap::{Arg, Command};
 
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let matches = Command::new("FlowLab")
+        .version("0.1.0")
+        .about("A monitoring and control system for scientific instruments")
+        .arg(
+            Arg::new("device")
+                .short('d')
+                .long("device")
+                .value_name("FILE")
+                .required(true)
+                .help("Device configuration file"),
+        )
+        .arg(
+            Arg::new("instruction")
+                .short('i')
+                .long("instruction")
+                .value_name("FOLDER")
+                .required(true)
+                .help("Instruction configuration folder"),
+        )
+        .arg(
+            Arg::new("pipeline")
+                .short('p')
+                .long("pipeline")
+                .value_name("FILE")
+                .required(true)
+                .help("Pipeline configuration file"),
+        )
+        .get_matches();
+}
